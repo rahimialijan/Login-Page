7c0324712932996bf38938a985d4b24e18c71c5d
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-03-19 23:06:01 +0500

    remove line spaces on js file

--- main.js
+++ main.js
@@ -6,32 +6,20 @@ const loginSection=document.querySelector('.login-page');
 const registerSection=document.querySelector('.register-container');
 const forgotSection=document.querySelector('.forgot-container');
 
-
-
 fogotPass.addEventListener('click', ()=>{
-
-
     loginSection.style.display='none';
     forgotSection.style.display='flex';
 })
 
 registerAccount.addEventListener('click', ()=>{
-
-
     loginSection.style.display='none';
     registerSection.style.display='flex';
 })
-
-
 login.addEventListener('click', ()=>{
-
     registerSection.style.display='none';
     loginSection.style.display='flex';
 })
-
 close.addEventListener('click', ()=>{
-
     forgotSection.style.display='none';
-    
     loginSection.style.display='flex';
 })
