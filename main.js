2fa212d62ec0e15d984edfe21848b73d64151d10
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-03-20 00:36:12 +0500

    fix main.js file error

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-const fogotPass=document.querySelector('.forgot-pass');
-const registerAccount=document.querySelector('.register-account');
-const login=document.querySelector('.login');
-const close=document.querySelector('.close');
-const loginSection=document.querySelector('.login-page');
-const registerSection=document.querySelector('.register-container');
-const forgotSection=document.querySelector('.forgot-container');
+const fogotPass = document.querySelector('.forgot-pass');
+const registerAccount = document.querySelector('.register-account');
+const login = document.querySelector('.login');
+const close = document.querySelector('.close');
+const loginSection = document.querySelector('.login-page');
+const registerSection = document.querySelector('.register-container');
+const forgotSection = document.querySelector('.forgot-container');
 
-fogotPass.addEventListener('click', ()=>{
-    loginSection.style.display='none';
-    forgotSection.style.display='flex';
-})
+fogotPass.addEventListener('click', () => {
+  loginSection.style.display = 'none';
+  forgotSection.style.display = 'flex';
+});
 
-registerAccount.addEventListener('click', ()=>{
-    loginSection.style.display='none';
-    registerSection.style.display='flex';
-})
-login.addEventListener('click', ()=>{
-    registerSection.style.display='none';
-    loginSection.style.display='flex';
-})
-close.addEventListener('click', ()=>{
-    forgotSection.style.display='none';
-    loginSection.style.display='flex';
-})
+registerAccount.addEventListener('click', () => {
+  loginSection.style.display = 'none';
+  registerSection.style.display = 'flex';
+});
+login.addEventListener('click', () => {
+  registerSection.style.display = 'none';
+  loginSection.style.display = 'flex';
+});
+close.addEventListener('click', () => {
+  forgotSection.style.display = 'none';
+  loginSection.style.display = 'flex';
+});
